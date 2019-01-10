e95368fa4526158ecb10f3db953b6910aba2ba95
Fran <hola@beachlab.org>
2019-01-10 23:57:23 +0100

    dict mess half solved

--- src/soundboard.py
+++ src/soundboard.py
@@ -2,7 +2,6 @@
 # being i row number and j column number (starting at 0)
 
 import pygame
-import json
 
 # variables
 size = [800, 800]
@@ -13,56 +12,44 @@ print(spacing)
 # initialize game engine
 pygame.mixer.pre_init(44100, -16, 1, 512)  # fixes delay in play
 pygame.init()
+
 # set screen width/height and caption
 screen = pygame.display.set_mode(size)  # , pygame.NOFRAME
 pygame.display.set_caption('pySoundBoard')
 
-data = {}  # init the soundboard data
-data['buttons'] = []
+# init fonts
+fontLogo = pygame.font.Font('res/Kathen.otf', int(spacing/1.5))
+fontObj = pygame.font.Font('res/Hyperspace.otf', int(spacing/2.5))
+
+# init the soundboard data (list of dictionaries)
+data = []
 
 # generate sound button objects
 for i in range(rows):
     for j in range(rows):
-        data['buttons'].append({
-            'row': i,
-            'column': j,
-            'number': i+j,
+        data.append({
+            'column': i,
+            'row': j,
             'text': 'empty',
             'sound': 'sounds/fallback.wav',
             'coord': (spacing*(2*i+1), spacing*(2*j+1)),
             'size': (spacing, spacing),
+            'rectobj': pygame.Rect(spacing*(2*i+1), spacing*(2*j+1), spacing, spacing),
+            'textobj': fontObj.render(str(i)+str(j), False, (0, 0, 0)),
+            'textcoords': (spacing*(2*i+1.5), spacing*(2*j+1.5)),
             'color': (150, 150, 150),
             'bordecolor': (255, 255, 0),
             'borderzise': (spacing+12, spacing+12),
             'loop': False
         })
 
-# save JSON file
-json.dump(data, open('data.json', 'w'))
-
-# logo
-fontLogo = pygame.font.Font('res/Kathen.otf', int(spacing/1.5))
+# draw logo
 logo = fontLogo.render('pySoundboard', True, (55, 55, 55))
 logoRect = logo.get_rect()
 logoRect.midright = (spacing*(2*rows), size[1]-spacing/2)
 
-# button texts
-# numbers = {}
-# fontObj = pygame.font.Font('res/Hyperspace.otf', int(spacing/2.5))
-# for i in range(rows**2):
-#     textObj_{i}{j} = fontObj.render(str({i})+str({j}), False, (0, 0, 0))
-#     # get the surrounding rectangle
-#     textRectObj_{i}{j} = textObj_{i}{j}.get_rect()
-#     # set the center of the rectangle
-#     textRectObj_{i}{j}.center = (spacing*(2*{i}+1.5), spacing*(2*{j}+1.5))
-
 # initialize clock. used later in the loop.
 clock = pygame.time.Clock()
-
-# create rectangle
-# for i in range(rows):
-#     for j in range(rows):
-#         pygame.Rect(spacing*(2*i+1), spacing*(2*j+1), spacing, spacing)
 
 # Loop until the user clicks close button
 done = False
@@ -74,18 +61,17 @@ while done == False:
     # clear the screen before drawing
     screen.fill((30, 30, 30))
     # write game logic here
-    # x, y = pygame.mouse.get_pos()
-    # for i in range(rows):
-    #     for j in range(rows):
-    #         exec(
-    #             f'if rect_{i}{j}.collidepoint(x, y):sound_{i}{j}.play();pygame.draw.rect(screen, (255,255,0), [rect_{i}{j}.x-6, rect_{i}{j}.y-6, spacing+12, spacing+12], 1)')
+    x, y = pygame.mouse.get_pos()
+    for elem in data:
+        if elem['rectobj'].collidepoint(x, y):
+            # sound_{i}{j}.play()
+            pygame.draw.rect(screen, (255, 255, 0), [
+                             elem['coord'][0]-6, elem['coord'][1]-6, spacing+12, spacing+12], 1)
     # write draw code here
     screen.blit(logo, logoRect)
-    for i in range(rows):
-        for j in range(rows):
-            pygame.draw.rect(screen, (150, 150, 150), pygame.Rect(
-                spacing*(2*i+1), spacing*(2*j+1), spacing, spacing))
-            # exec(f'screen.blit(textObj_{i}{j}, textRectObj_{i}{j})')
+    for elem in data:
+        pygame.draw.rect(screen, elem['color'], elem['rectobj'])
+        screen.blit(elem['textobj'], elem['textcoords'])
 
     # display what’s drawn. this might change.
     pygame.display.update()
