1ff72f6ebd4a756f5ebc11ec2b7c61c5e946a6c0
Fran <hola@beachlab.org>
2019-01-11 15:41:28 +0100

    wrapped code in functions

--- src/soundboard.py
+++ src/soundboard.py
@@ -21,39 +21,55 @@ pygame.display.set_caption('pySoundBoard')
 fontLogo = pygame.font.Font('res/Kathen.otf', int(spacing/1.5))
 fontObj = pygame.font.Font('res/Hyperspace.otf', int(spacing/2.5))
 
-paths = []
-# read rows**2 lines of paths from file
-with open("paths.txt") as myfile:
-    paths = [next(myfile) for x in range(rows**2)]
-# remove white space
-paths = [line.rstrip('\n') for line in paths]
 
-# init the soundboard data (list of dictionaries)
-data = []
+def readpaths():
+    '''read rows**2 lines from paths.txt file
+    and create a list of paths'''
+    paths = []
+    with open("paths.txt") as myfile:
+        paths = [next(myfile) for x in range(rows**2)]
+    # remove white space
+    paths = [line.rstrip('\n') for line in paths]
+    return paths
 
-# generate sound button objects
-for i in range(rows):
-    for j in range(rows):
-        data.append({
-            'column': i,
-            'row': j,
-            'text': 'empty',
-            'soundobj': pygame.mixer.Sound(paths[i+j]),
-            'coord': (spacing*(2*i+1), spacing*(2*j+1)),
-            'size': (spacing, spacing),
-            'rectobj': pygame.Rect(spacing*(2*i+1), spacing*(2*j+1), spacing, spacing),
-            'textobj': fontObj.render(str(i)+str(j), False, (0, 0, 0)),
-            'textcoords': (spacing*(2*i+1.5), spacing*(2*j+1.5)),
-            'color': (150, 150, 150),
-            'bordercolor': (255, 255, 0),
-            'bordersize': (spacing+12, spacing+12),
-            'loop': False
-        })
 
-# draw logo
-logo = fontLogo.render('pySoundboard', True, (55, 55, 55))
-logoRect = logo.get_rect()
-logoRect.midright = (spacing*(2*rows), size[1]-spacing/2)
+def makebuttons():
+    '''generate sound button objects according to the number of
+    rows'''
+    data = []
+    for i in range(rows):
+        for j in range(rows):
+            data.append({
+                'column': i,
+                'row': j,
+                'text': 'empty',
+                'soundobj': pygame.mixer.Sound(paths[i+j]),
+                'coord': (spacing*(2*i+1), spacing*(2*j+1)),
+                'size': (spacing, spacing),
+                'rectobj': pygame.Rect(spacing*(2*i+1), spacing*(2*j+1), spacing, spacing),
+                'textobj': fontObj.render(str(i)+str(j), False, (0, 0, 0)),
+                'textcoords': (spacing*(2*i+1.5), spacing*(2*j+1.5)),
+                'color': (150, 150, 150),
+                'bordercolor': (255, 255, 0),
+                'bordersize': (spacing+12, spacing+12),
+                'loop': False
+            })
+    return data
+
+
+def makelogo():
+    # draw logo according to the size of the buttons
+    logo = fontLogo.render('pySoundboard', True, (55, 55, 55))
+    logoRect = logo.get_rect()
+    logoRect.midright = (spacing*(2*rows), size[1]-spacing/2)
+    print(logo)
+    return (logo, logoRect, logoRect.midright)
+
+
+# make the initial set of objects
+paths = readpaths()
+data = makebuttons()
+logo = makelogo()
 
 # initialize clock. used later in the loop.
 clock = pygame.time.Clock()
@@ -81,7 +97,7 @@ while done == False:
             pygame.draw.rect(screen, elem['bordercolor'], (elem['coord'][0]-6,
                                                            elem['coord'][1]-6, elem['bordersize'][0], elem['bordersize'][1]), 1)
     # write draw code here
-    screen.blit(logo, logoRect)
+    screen.blit(logo[0], logo[1])
     for elem in data:
         pygame.draw.rect(screen, elem['color'], elem['rectobj'])
         screen.blit(elem['textobj'], elem['textcoords'])
